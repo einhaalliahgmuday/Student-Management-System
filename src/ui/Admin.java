055ae708f5a8cacdb01e04d930b49d87f181f244
Einha Alliah Muday <einhalliahmuday@gmail.com>
2023-06-30 18:56:11 +0800

    created class for Admin UI

---
+++ src/ui/Admin.java
@@ -0,0 +1,22 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Admin extends JFrame {
+	
+	Admin() {
+		
+		setTitle("Student Management System");
+		setSize(1050, 650);
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		
+		setVisible(true);
+	}
+
+}
